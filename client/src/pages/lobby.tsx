cfb68627c8a10ee7d078a2a405a06a8d7aad75b3
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-07-21 21:40:37 +0530

    chore: separate card for both create and join

--- client/src/pages/lobby.tsx
+++ client/src/pages/lobby.tsx
@@ -1,46 +1,84 @@
+import { useState } from "react";
 import { Button } from "../components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../components/ui/card";
 import { Input } from "../components/ui/input";
 import { Label } from "../components/ui/label";
+import { Tabs, TabsList, TabsTrigger } from "../components/ui/tabs";
+import { TabsContent } from "@radix-ui/react-tabs";
+
 
 export function Lobby () {
+	const [username, setUsername] = useState("");
+	const [roomcode, setRoomcode] = useState("");
+
     return (
-      
-        <div className="w-screen h-screen flex items-center justify-center">
-            <Card className="w-full max-w-sm">
-				<CardHeader>
-					<CardTitle>Welcome </CardTitle>
-					<CardDescription>
-						You can create a room or join one
-					</CardDescription>
-				</CardHeader>
-				<CardContent>
-					<div className="flex flex-col gap-4">
-						<div className="grid gap-2">
-						<Label htmlFor="roomcode">Enter room code</Label>
-						<Input
-							id="roomcode"
-							type="roomcode"
-							placeholder="Enter room code"
-							required
-						/>
-						</div>
-						<Button variant={"default"} className="w-full">
-						Join
-						</Button>
-					</div>
-				</CardContent>
-				<div className="flex items-center justify-center px-6">
-					<hr className="flex-grow border-t border-gray-300" />
-					<span className="px-4 text-gray-500">or</span>
-					<hr className="flex-grow border-t border-gray-300" />
-				</div>
-				<CardFooter>
-					<Button variant="outline" className="w-full">
-					Create a room
-					</Button>
-				</CardFooter>
-			</Card>
+		<div className="w-screen h-screen flex flex-col items-center justify-center">
+			<div className="flex w-full max-w-sm flex-col gap-6">
+				<Tabs defaultValue="create">
+					<TabsList>
+						<TabsTrigger value="create">Create</TabsTrigger>
+						<TabsTrigger value="join">Join</TabsTrigger>
+					</TabsList>
+					<TabsContent value="create">
+						<Card className="w-full max-w-2xl">
+							<CardHeader>
+								<CardTitle>Welcome </CardTitle>
+								<CardDescription>
+									You can create a room or join one
+								</CardDescription>
+							</CardHeader>
+							<CardContent>
+								<div className="flex flex-col gap-4">
+									<div className="grid gap-2">
+										<Label htmlFor="name">Enter you name</Label>
+										<Input
+											id="name"
+											type="text"
+											placeholder="Enter you name"
+										/>
+									</div>
+									<Button variant={"default"} className="w-full">
+									Create
+									</Button>
+								</div>
+							</CardContent>
+						</Card>
+					</TabsContent>
+					<TabsContent value="join">
+						<Card className="w-full max-w-2xl">
+							<CardHeader>
+								<CardTitle>Welcome </CardTitle>
+								<CardDescription>
+									You can create a room or join one
+								</CardDescription>
+							</CardHeader>
+							<CardContent>
+								<div className="flex flex-col gap-4">
+									<div className="grid gap-2">
+										<Label htmlFor="name">Enter you name</Label>
+										<Input
+											id="name"
+											type="text"
+											placeholder="Enter you name"
+										/>
+									</div>
+									<div className="grid gap-2">
+										<Label htmlFor="roomcode">Enter room code</Label>
+										<Input
+											id="roomcode"
+											type="text"
+											placeholder="Enter room code"
+										/>
+									</div>
+									<Button variant={"default"} className="w-full">
+									Join
+									</Button>
+								</div>
+							</CardContent>
+						</Card>
+					</TabsContent>
+				</Tabs>
+			</div>
         </div>
     )
 }
