f12ead17a9138d1a2c36bd631fea6570a8487dd7
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-07-21 21:38:31 +0530

    chore: add page routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Lobby } from "./pages/lobby"
+import { Room } from "./pages/room"
 
 
 function App() {
 
   return (
-    <>
-    Hello
-    </>
-  )
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Lobby />} />
+				<Route path="/room:id" element={<Room />} />
+			</Routes>
+		</BrowserRouter>
+	)
 }
 
 export default App
